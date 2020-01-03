85cdb86977dfe93b9ef6d243e821cd74c1fb2c83
Egor.Ruban <Egor-Ruban@mail.ru>
2020-01-04 02:15:45 +0700

    добавил генерацию случайного уровня на стартовом экране

--- app/src/main/java/ru/tsu/killthemole/MainActivity.kt
+++ app/src/main/java/ru/tsu/killthemole/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.tsu.killthemole
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,15 +9,25 @@ import android.widget.SeekBar
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_settings.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
+        //TODO создать статистику
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btn_play.setOnClickListener{
-            //TODO сделать рандом или запуск непройденного уровня
             val intent = Intent(this, GameActivity::class.java)
+            val holes = Random.nextInt(1,21)
+            val difficulty = Random.nextInt(1,holes)
+            val speed = Random.nextLong(1000L,3000L)
+            with(intent){
+                putExtra("holes",holes)
+                putExtra("difficulty", difficulty)
+                putExtra("speed", speed)
+
+            }
             startActivity(intent)
         }
         btn_levels.setOnClickListener {
